97ebbfbfe2ab37cfbd91f75055c33c9d9547ddf6
Liang Tao <amethyst.black@gmail.com>
2012-08-31 10:30:30 +0800

    Add tag mechenism and the unset() function.

--- lib/QBox/Debug.pm
+++ lib/QBox/Debug.pm
@@ -13,23 +13,51 @@ package QBox::Debug;
 use strict;
 use warnings;
 
-my $qbox_handler = undef;
-my $qbox_data    = undef;
+use constant TAG_ALL => 'all';
+
+my %callback_settings = ();
 
 ### for OOP
 sub set_callback {
     my $handler = shift;
     my $data    = shift;
+    my $tag     = shift;
+    
+    $tag = defined($tag) ? "$tag" : TAG_ALL;
 
     if (ref($handler) eq q{CODE}) {
-        $qbox_handler = $handler;
-        $qbox_data    = $data;
+        $callback_settings{$tag} = {
+            handler => $handler,
+            data    => $data,
+        };
+        return 1;
     }
+
+    return undef;
 } # set_callback
 
+sub unset_callback {
+    my $tag = shift || TAG_ALL;
+
+    $tag = defined($tag) ? "$tag" : TAG_ALL;
+
+    if (exists($callback_settings{$tag})) {
+        delete($callback_settings{$tag});
+    }
+} # unset_callback
+
 sub callback {
-    if ($qbox_handler) {
-        $qbox_handler->($qbox_data, @_);
+    my ($tag) = @_;
+
+    my $setting = $callback_settings{$tag};
+    if (defined($setting)) {
+        my $ret = $setting->{handler}->($setting->{data}, @_);
+        return if not $ret; # no propogation
+    }
+
+    $setting =  $callback_settings{+TAG_ALL};
+    if (defined($setting)) {
+        $setting->{handler}->($setting->{data}, @_);
     }
 } # callback
 
